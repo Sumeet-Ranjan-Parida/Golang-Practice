277e8a195ab5d769d22b49cabe58afc8ab4caf4c
Sumeet Ranjan Parida <42038709+Sumeet-Ranjan-Parida@users.noreply.github.com>
2020-10-22 17:13:02 +0530

    Update functions.go

--- functions.go
+++ functions.go
@@ -6,6 +6,11 @@ func greetings(name string) string {
 	return "Hello, " + name
 }
 
+func add(num1 int, num2 int) int {
+	return num1 + num2
+}
+
 func main() {
 	fmt.Println(greetings("Sumeet"))
+	fmt.Println(add(2, 2))
 }
