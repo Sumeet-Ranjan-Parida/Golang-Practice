35abea7e09e884d3318c2c9016a36e52af8cf329
Sumeet Ranjan Parida <42038709+Sumeet-Ranjan-Parida@users.noreply.github.com>
2020-10-23 18:10:54 +0530

    Add files via upload

---
+++ array_slices.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [2]int{4, 5}
+
+	fmt.Println(arr)
+
+	slice := []string{"a", "b"}
+
+	fmt.Println(slice)
+}
---
+++ if_else.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+	y := 15
+
+	if x > y {
+		fmt.Println("x is greater than y")
+	} else {
+		fmt.Println("x is less than y")
+	}
+}
