4074be935e0df7327fef52730c06345b80bed179
Sumeet Ranjan Parida <psumeet1998@gmail.com>
2020-11-17 16:12:37 +0530

    commit

---
+++ Program/ascii_art.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	t "time"
+)
+
+func main() {
+	fmt.Println("A Dog")
+
+	fmt.Println("  __      _")
+	fmt.Println("o'')}____//")
+	fmt.Println(" `_/      )")
+	fmt.Println(" (_(_/-(_/ ")
+
+	fmt.Printf("\nDone by Sumeet ")
+	fmt.Println(t.Now())
+}
